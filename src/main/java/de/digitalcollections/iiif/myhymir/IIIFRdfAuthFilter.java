05f43e8f5a3cc3351b7d6d5235e808ca40bade6d
Marc Agate <agate.marc@gmail.com>
2019-04-08 09:22:33 -0400

    Fixing cookie parsing

--- src/main/java/de/digitalcollections/iiif/myhymir/IIIFRdfAuthFilter.java
+++ src/main/java/de/digitalcollections/iiif/myhymir/IIIFRdfAuthFilter.java
@@ -22,12 +22,11 @@ import io.bdrc.auth.TokenValidation;
 import io.bdrc.auth.UserProfile;
 import io.bdrc.auth.model.Endpoint;
 
-
 @Component
 @Order(1)
-public class IIIFRdfAuthFilter implements Filter{
+public class IIIFRdfAuthFilter implements Filter {
 
-    public final static Logger log=LoggerFactory.getLogger(IIIFRdfAuthFilter.class.getName());
+    public final static Logger log = LoggerFactory.getLogger(IIIFRdfAuthFilter.class.getName());
 
     @Override
     public void destroy() {
@@ -35,36 +34,37 @@ public class IIIFRdfAuthFilter implements Filter{
     }
 
     @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-            throws IOException, ServletException {
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 
-        String token=getToken(((HttpServletRequest)req).getHeader("Authorization"));
-        if(token==null) {
-            Cookie[] cookies=((HttpServletRequest)req).getCookies();
-            if(cookies!=null) {
-                for(Cookie cook:cookies) {
-                    if(cook.getName().equals("Set-Cookie")) {
-                        String ck=cook.getValue();
-                        String[] parts=ck.split(";");
-                        for(String part:parts) {
-                            if(cook.getName().equals(AuthProps.getProperty("cookieKey"))) {
-                                token=part.split("=")[0];
-                                break;
-                            }
-                        }
+        String token = getToken(((HttpServletRequest) req).getHeader("Authorization"));
+        if (token == null) {
+            Cookie[] cookies = ((HttpServletRequest) req).getCookies();
+            log.info("Cookies in filter >>" + cookies);
+            if (cookies != null) {
+                for (Cookie cook : cookies) {
+                    // if(cook.getName().equals("Set-Cookie")) {
+                    // String ck=cook.getValue();
+                    // String[] parts=ck.split(";");
+                    // for(String part:parts) {
+                    if (cook.getName().equals(AuthProps.getProperty("cookieKey"))) {
+                        token = cook.getValue();
+                        log.info("Token from Cookies in filter >>" + token);
+                        break;
                     }
+                    // }
+                    // }
                 }
             }
         }
-        TokenValidation validation=null;
-        UserProfile prof=null;
-        if(token !=null) {
-            //User is logged in
-            //Getting his profile
-            validation=new TokenValidation(token);
-            prof=validation.getUser();
-            req.setAttribute("access", new Access(prof,new Endpoint()));
-        }else {
+        TokenValidation validation = null;
+        UserProfile prof = null;
+        if (token != null) {
+            // User is logged in
+            // Getting his profile
+            validation = new TokenValidation(token);
+            prof = validation.getUser();
+            req.setAttribute("access", new Access(prof, new Endpoint()));
+        } else {
             req.setAttribute("access", new Access());
         }
         chain.doFilter(req, res);
@@ -77,11 +77,10 @@ public class IIIFRdfAuthFilter implements Filter{
 
     String getToken(String header) {
         try {
-            if(header!=null) {
+            if (header != null) {
                 return header.split(" ")[1];
             }
-        }
-        catch(Exception ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
             log.error(ex.getMessage());
             return null;
