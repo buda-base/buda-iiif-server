d209456bd09bc042dd376cadccebfadbb02db156
Marc Agate <agate.marc@gmail.com>
2019-06-05 11:20:18 -0400

    Tracking geoLocation

--- src/main/java/de/digitalcollections/iiif/myhymir/GeoLocation.java
+++ src/main/java/de/digitalcollections/iiif/myhymir/GeoLocation.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.maxmind.geoip2.DatabaseReader;
 import com.maxmind.geoip2.exception.GeoIp2Exception;
 import com.maxmind.geoip2.model.CountryResponse;
@@ -16,6 +19,7 @@ public class GeoLocation {
     private static final String DBLocation = AuthProps.getProperty("geolite_countryDB");
     private static DatabaseReader dbReader;
     public static final String GEO_CACHE_KEY = "GeoDB";
+    private static final Logger log = LoggerFactory.getLogger(GeoLocation.class);
 
     public static String getCountryName(String ip) {
         try {
@@ -26,9 +30,11 @@ public class GeoLocation {
                 ServerCache.addToCache("info", GEO_CACHE_KEY, dbReader);
             }
             InetAddress ipAddress = InetAddress.getByName(ip);
+            log.info("IP = {}", ipAddress);
             CountryResponse response = dbReader.country(ipAddress);
             return response.getCountry().getName();
         } catch (IOException | BDRCAPIException | GeoIp2Exception e) {
+            log.info("GeoLocation exception {}", e.getMessage());
             e.printStackTrace();
             return null;
         }
