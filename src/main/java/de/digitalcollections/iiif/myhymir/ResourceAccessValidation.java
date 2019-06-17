7ba1d9fd891e421d1cd88169b3783efba0a1854e
Marc Agate <agate.marc@gmail.com>
2019-06-17 14:57:32 -0400

    attempt to configure spring boot for remote Ip forwarding

--- src/main/java/de/digitalcollections/iiif/myhymir/ResourceAccessValidation.java
+++ src/main/java/de/digitalcollections/iiif/myhymir/ResourceAccessValidation.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.http.client.ClientProtocolException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import de.digitalcollections.iiif.hymir.model.exception.ResourceNotFoundException;
 import io.bdrc.auth.Access;
@@ -15,6 +17,7 @@ import io.bdrc.iiif.resolver.IdentifierInfo;
 public class ResourceAccessValidation {
 
     private static final String CHINA = "China";
+    private static final Logger log = LoggerFactory.getLogger(ResourceAccessValidation.class);
 
     Access access;
     String accessType;
@@ -44,7 +47,7 @@ public class ResourceAccessValidation {
     }
 
     public boolean isAccessible(HttpServletRequest request) {
-        System.out.println("TEST >>" + GeoLocation.getCountryName(request.getHeader("X-Real-IP")));
+        log.info("TEST IP from X-Real-IP header: {} and country: {}", request.getHeader("X-Real-IP"), GeoLocation.getCountryName(request.getHeader("X-Real-IP")));
         if (access == null) {
             access = new Access();
         }
