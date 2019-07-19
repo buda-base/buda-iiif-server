95bf7dfbfa7922b32d72594f33aff773e28012dd
Elie Roux <elie.roux@telecom-bretagne.eu>
2019-07-19 09:13:33 +0200

    prepare for test

---
+++ src/test/java/io/bdrc/iiif/PDFBugDemo.java
@@ -0,0 +1,15 @@
+package io.bdrc.iiif;
+
+public class PDFBugDemo {
+
+    static void testIText() {
+        // generate a one page PDF with itext from src/test/resources/default.tif
+        // output pdf in /tmp/testitext.pdf
+    }
+    
+    static void testApachePDFBox() {
+        // generate a one page PDF with Apache PDF box from src/test/resources/default.tif
+        // output pdf in /tmp/testapache.pdf
+    }
+    
+}
