225a8bcacf7325f9221105e2e8fd46cbc2b51f80
Marc Agate <agate.marc@gmail.com>
2019-09-09 09:09:02 -0400

    addressing issue #37

--- src/main/java/de/digitalcollections/iiif/myhymir/GeoLocation.java
+++ src/main/java/de/digitalcollections/iiif/myhymir/GeoLocation.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,34 +14,45 @@ import com.maxmind.geoip2.exception.GeoIp2Exception;
 import com.maxmind.geoip2.model.CountryResponse;
 
 import io.bdrc.auth.AuthProps;
-import io.bdrc.iiif.exceptions.IIIFException;
 
 public class GeoLocation {
 
     private static final String DBLocation = AuthProps.getProperty("geolite_countryDB");
-    private static DatabaseReader dbReader;
     public static final String GEO_CACHE_KEY = "GeoDB";
-    public static final Logger custom = LoggerFactory.getLogger("custom");
+    private static final String CHINA = "China";
+    public static final String HEADER_NAME = "X-Real-IP";
+    private static final Logger log = LoggerFactory.getLogger(GeoLocation.class);
+    private static DatabaseReader dbReader = getDbReader();
 
-    public static String getCountryName(String ip) {
-        InetAddress ipAddress = null;
+    public static DatabaseReader getDbReader() {
         try {
-            dbReader = (DatabaseReader) ServerCache.getObjectFromCache("info", GEO_CACHE_KEY);
-            if (dbReader == null) {
-                File database = new File(DBLocation);
-                dbReader = new DatabaseReader.Builder(database).build();
-                ServerCache.addToCache("info", GEO_CACHE_KEY, dbReader);
-            }
-            ipAddress = InetAddress.getByName(ip);
-            custom.info("IP = {}", ipAddress);
-            CountryResponse response = dbReader.country(ipAddress);
-            return response.getCountry().getName();
-        } catch (IOException | IIIFException | GeoIp2Exception e) {
-            custom.info("GeoLocation exception {}", e.getMessage());
-            if (!ipAddress.toString().contains("127.0.0.1")) {
-                e.printStackTrace();
-            }
+            File database = new File(DBLocation);
+            return new DatabaseReader.Builder(database).build();
+        } catch (IOException e) {
+            log.error("getDbReader()", e);
             return null;
         }
+    }
+
+    public static String getCountryName(String ip) {
+        try {
+            final InetAddress ipAddress = InetAddress.getByName(ip);
+            final CountryResponse response = dbReader.country(ipAddress);
+            return response.getCountry().getName();
+        } catch (IOException | GeoIp2Exception e) {
+            log.error("getCountryName()", e);
+            return null;
+        }
+    }
+
+    public static boolean isFromChina(final String addr) {
+        final String country = GeoLocation.getCountryName(addr);
+        log.debug("For address {}, country is {}", addr, country);
+        return (country == null || CHINA.equalsIgnoreCase(country));
+    }
+
+    public static boolean isFromChina(HttpServletRequest request) {
+        final String addr = request.getHeader(HEADER_NAME);
+        return isFromChina(addr);
     }
 }
