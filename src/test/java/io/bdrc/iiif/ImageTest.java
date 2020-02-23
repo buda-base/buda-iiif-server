9e2c1f6af97ad23258fa528d4e508fc9a90d5eaf
eroux <elie.roux@telecom-bretagne.eu>
2020-02-23 12:21:47 +0100

    try to write the icc, fails

--- src/test/java/io/bdrc/iiif/ImageTest.java
+++ src/test/java/io/bdrc/iiif/ImageTest.java
@@ -8,9 +8,12 @@ import java.io.InputStream;
 import java.io.StringWriter;
 import java.util.Iterator;
 
+import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageReadParam;
 import javax.imageio.ImageReader;
 import javax.imageio.ImageTypeSpecifier;
+import javax.imageio.ImageWriteParam;
 import javax.imageio.ImageWriter;
 import javax.imageio.event.IIOReadWarningListener;
 import javax.imageio.metadata.IIOMetadata;
@@ -26,6 +29,8 @@ import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Node;
 
+import com.twelvemonkeys.imageio.plugins.jpeg.JPEGImageWriter;
+import com.twelvemonkeys.imageio.plugins.jpeg.JPEGImageWriterSpi;
 
 import de.digitalcollections.iiif.hymir.model.exception.UnsupportedFormatException;
 
@@ -58,36 +63,36 @@ public class ImageTest {
         
         // empty <app2ICC/>
         printMetadata(r.getImageMetadata(0));
+        
         ImageTypeSpecifier its = r.getRawImageType(0);
+        ImageReadParam p = r.getDefaultReadParam();
+        p.setDestinationType(its);
         
         // color space is RGB (not sRGB), not sure if it's relevant
         System.out.println("is ColorSpace RGB? "+(its.getColorModel().getColorSpace().getType() == ColorSpace.TYPE_RGB));
                 
-        BufferedImage bi = r.read(0);
+        BufferedImage bi = r.read(0, p);
         
-        // no writer for the raw image type of the input image:
+        // if I try to find a writer handling the raw image type, I don't get any:
         Iterator<ImageWriter> itw = ImageIO.getImageWriters(its, "jpeg");
         ImageWriter iw = itw.next();
         if (iw == null) {
             System.out.println("no writer for the raw image type of the input");
-        } else {
-            ImageOutputStream out = ImageIO.createImageOutputStream(new File("test-inputrawimagetype.jpg"));
-            iw.setOutput(out);
-            iw.write(bi);
         }
         
-        // when writing with a more regular image type, the icc profile is not kept
+        // nor do I when I try to get a writer for the output of the read:
         Iterator<ImageWriter> itw2 = ImageIO.getImageWriters(new ImageTypeSpecifier(bi), "jpeg");
         ImageWriter iw2 = itw2.next();
         if (iw2 == null) {
             System.out.println("no writer for the image type of the buffered image");
         } else {
+            ImageWriteParam wp = iw2.getDefaultWriteParam();
+            wp.setDestinationType(its);
             ImageOutputStream out = ImageIO.createImageOutputStream(new File("test-regularjpg.jpg"));
-            //iw2.
             iw2.setOutput(out);
-            iw2.write(r.read(0));
+            iw2.write(bi);
         }
-            
+        
         is.close();
         iis.close();
     }
