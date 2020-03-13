423f0f2d3afb3255cb2931f39b6e6fc441963c97
Marc Agate <agate.marc@gmail.com>
2020-03-13 08:30:12 -0400

    Now using EHCache in JCSNode Cache - cleanup

--- src/main/java/de/digitalcollections/iiif/myhymir/JCSNodeCache.java
+++ src/main/java/de/digitalcollections/iiif/myhymir/JCSNodeCache.java
@@ -2,8 +2,12 @@ package de.digitalcollections.iiif.myhymir;
 
 import java.io.IOException;
 
-import org.apache.commons.jcs.JCS;
-import org.apache.commons.jcs.access.CacheAccess;
+import org.ehcache.Cache;
+import org.ehcache.CacheManager;
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.CacheManagerBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.config.units.EntryUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,24 +15,29 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.maxmind.db.NodeCache;
 
 public class JCSNodeCache implements NodeCache {
-    
+
     // The raison d'être of this class is to be used by Geoloc's cache mechanism
 
-    private CacheAccess<String, Object> jcscache;
+    public static Cache<String, JsonNode> CACHE;
     public final static Logger log = LoggerFactory.getLogger(JCSNodeCache.class.getName());
 
     public JCSNodeCache() {
-        jcscache = JCS.getInstance("geoloc");
+        CacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder().build();
+        cacheManager.init();
+        CACHE = cacheManager.createCache("geoloc", CacheConfigurationBuilder.newCacheConfigurationBuilder(String.class, JsonNode.class,
+                ResourcePoolsBuilder.newResourcePoolsBuilder().heap(500, EntryUnit.ENTRIES)));
+        log.debug("Cache was initialized {}", CACHE);
+
     }
 
     @Override
     public JsonNode get(int key, Loader loader) throws IOException {
         try {
             String k = Integer.toString(key);
-            JsonNode value = (JsonNode) jcscache.get(k);
+            JsonNode value = CACHE.get(k);
             if (value == null) {
                 value = loader.load(key);
-                jcscache.put(k, value);
+                CACHE.put(k, value);
             }
             return value;
         } catch (IOException e) {
