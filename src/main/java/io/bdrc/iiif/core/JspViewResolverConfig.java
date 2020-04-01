331f0e1a4ecb36a9eef60dd873ba5251665fda4c
Marc Agate <agate.marc@gmail.com>
2020-04-01 13:16:41 -0400

    removed ThymeLeaf resolver bean

--- src/main/java/io/bdrc/iiif/core/JspViewResolverConfig.java
+++ src/main/java/io/bdrc/iiif/core/JspViewResolverConfig.java
@@ -1,16 +1,12 @@
 package io.bdrc.iiif.core;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
-@EnableAutoConfiguration
-@EnableWebMvc
 public class JspViewResolverConfig {
 
     @Bean
