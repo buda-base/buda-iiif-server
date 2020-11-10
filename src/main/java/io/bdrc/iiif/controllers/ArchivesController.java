0b86765dfa9ea4fc77eaca510ef00ca899b172af
MarcAgate <agate.marc@gmail.com>
2020-11-10 15:51:21 +0100

    Using properties for setting non relative download links dynamically

--- src/main/java/io/bdrc/iiif/controllers/ArchivesController.java
+++ src/main/java/io/bdrc/iiif/controllers/ArchivesController.java
@@ -32,6 +32,7 @@ import io.bdrc.auth.Access.AccessLevel;
 import io.bdrc.iiif.archives.ArchiveBuilder;
 import io.bdrc.iiif.archives.PdfItemInfo;
 import io.bdrc.iiif.auth.ResourceAccessValidation;
+import io.bdrc.iiif.core.Application;
 import io.bdrc.iiif.core.EHServerCache;
 import io.bdrc.iiif.exceptions.IIIFException;
 import io.bdrc.iiif.image.service.ImageInfoListService;
@@ -48,9 +49,10 @@ public class ArchivesController {
 
     public final static Logger log = LoggerFactory.getLogger(ArchivesController.class.getName());
 
-    @RequestMapping(value = "/download/{type}/{id}", method = { RequestMethod.GET, RequestMethod.HEAD })
-    public ResponseEntity<String> getPdfLink(@PathVariable String id, @PathVariable String type, HttpServletRequest request) throws Exception {
-        log.info("getPdfLink(id {}, type{})",id, type);
+    @RequestMapping(value = "/download/{type}/{id}", method = {RequestMethod.GET, RequestMethod.HEAD})
+    public ResponseEntity<String> getPdfLink(@PathVariable String id, @PathVariable String type,
+            HttpServletRequest request) throws Exception {
+        log.info("getPdfLink(id {}, type{})", id, type);
         Access acc = (Access) request.getAttribute("access");
         if (acc == null) {
             acc = new Access();
@@ -67,85 +69,90 @@ public class ArchivesController {
         String html = "";
         int subType = idf.getSubType();
         switch (subType) {
-        // Case work in item
-        case Identifier.MANIFEST_ID_WORK_IN_ITEM:
-            PdfItemInfo item = PdfItemInfo.getPdfItemInfo(idf.getImageInstanceId());
-            if (!acc.hasResourceAccess(item.getItemAccess())) {
-                return new ResponseEntity<>("Insufficient rights", HttpStatus.FORBIDDEN);
-            }
-            if (json) {
-                jsonMap = getJsonVolumeLinks(item, type);
-                ObjectMapper mapper = new ObjectMapper();
-                html = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonMap);
-            } else {
-                map.put("links", getVolumeDownLoadLinks(item, idf, type));
-                html = getTemplate("volumes.tpl");
-                s = new StringSubstitutor(map);
-                html = s.replace(html);
-            }
-            break;
-        // Case volume imageRange
-        case Identifier.MANIFEST_ID_VOLUMEID:
-        case Identifier.MANIFEST_ID_WORK_IN_VOLUMEID:
-            IdentifierInfo inf = new IdentifierInfo(idf.getImageGroupId());
-            Integer bPage = idf.getBPageNum();
-            if (bPage == null) {
-                bPage = 1;
-            }
-            Integer ePage = idf.getEPageNum();
-            if (ePage == null) {
-                ePage = inf.getTotalPages();
-            }
-            if (ePage < bPage) {
-                return new ResponseEntity<>("PDF would be empty", HttpStatus.NOT_FOUND);
-            }
-            log.info("Pdf requested numPage in identifierInfo {}", inf);
-            log.info("Pdf requested start page {} and end page {}", bPage.intValue(), ePage.intValue());
+            // Case work in item
+            case Identifier.MANIFEST_ID_WORK_IN_ITEM :
+                PdfItemInfo item = PdfItemInfo.getPdfItemInfo(idf.getImageInstanceId());
+                if (!acc.hasResourceAccess(item.getItemAccess())) {
+                    return new ResponseEntity<>("Insufficient rights", HttpStatus.FORBIDDEN);
+                }
+                if (json) {
+                    jsonMap = getJsonVolumeLinks(item, type);
+                    ObjectMapper mapper = new ObjectMapper();
+                    html = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonMap);
+                } else {
+                    map.put("links", getVolumeDownLoadLinks(item, idf, type));
+                    html = getTemplate("volumes.tpl");
+                    s = new StringSubstitutor(map);
+                    html = s.replace(html);
+                }
+                break;
+            // Case volume imageRange
+            case Identifier.MANIFEST_ID_VOLUMEID :
+            case Identifier.MANIFEST_ID_WORK_IN_VOLUMEID :
+                IdentifierInfo inf = new IdentifierInfo(idf.getImageGroupId());
+                Integer bPage = idf.getBPageNum();
+                if (bPage == null) {
+                    bPage = 1;
+                }
+                Integer ePage = idf.getEPageNum();
+                if (ePage == null) {
+                    ePage = inf.getTotalPages();
+                }
+                if (ePage < bPage) {
+                    return new ResponseEntity<>("PDF would be empty", HttpStatus.NOT_FOUND);
+                }
+                log.info("Pdf requested numPage in identifierInfo {}", inf);
+                log.info("Pdf requested start page {} and end page {}", bPage.intValue(), ePage.intValue());
 
-            List<ImageInfo> ili = null;
-            try {
-                ili = ImageInfoListService.Instance.getAsync(inf.igi.imageInstanceId.substring(AppConstants.BDR_len), inf.igi.imageGroup).get();
-            } catch (InterruptedException | ExecutionException e) {
-                throw new IIIFException(404, 5000, e);
-            }
-            ResourceAccessValidation accValidation = new ResourceAccessValidation((Access) request.getAttribute("access"), inf);
-            AccessLevel al = accValidation.getAccessLevel(request);
-            if (al.equals(AccessLevel.NOACCESS) || al.equals(AccessLevel.MIXED)) {
-                return new ResponseEntity<>("Insufficient rights", HttpStatus.FORBIDDEN);
-            }
-            if (al.equals(AccessLevel.FAIR_USE)) {
-                output = idf.getImageGroupId() + "FAIR_USE:" + bPage.intValue() + "-" + ePage.intValue();// +"."+type;
-            } else {
-                output = idf.getImageGroupId() + ":" + bPage.intValue() + "-" + ePage.intValue();// +"."+type;
-            }
-            if (type.equals(ArchiveBuilder.PDF_TYPE)) {
-                Object pdf_cached = EHServerCache.IIIF.get(output);
-                log.debug("PDF " + id + " from IIIF cache >>" + pdf_cached);
-                if (pdf_cached == null) {
-                    // Build pdf since the pdf file doesn't exist yet
-                    ArchiveBuilder.buildPdf(accValidation.getAccess(), inf, idf, output, (String) request.getAttribute("origin"));
+                List<ImageInfo> ili = null;
+                try {
+                    ili = ImageInfoListService.Instance
+                            .getAsync(inf.igi.imageInstanceId.substring(AppConstants.BDR_len), inf.igi.imageGroup)
+                            .get();
+                } catch (InterruptedException | ExecutionException e) {
+                    throw new IIIFException(404, 5000, e);
                 }
-            }
-            if (type.equals(ArchiveBuilder.ZIP_TYPE)) {
-                Object zip_cached = EHServerCache.IIIF_ZIP.get(output);
-                log.debug("ZIP " + id + " from IIIF_ZIP cache >>" + zip_cached);
-                if (zip_cached == null) {
-                    // Build pdf since the pdf file doesn't exist yet
-                    ArchiveBuilder.buildZip(accValidation.getAccess(), inf, idf, output, (String) request.getAttribute("origin"));
+                ResourceAccessValidation accValidation = new ResourceAccessValidation(
+                        (Access) request.getAttribute("access"), inf);
+                AccessLevel al = accValidation.getAccessLevel(request);
+                if (al.equals(AccessLevel.NOACCESS) || al.equals(AccessLevel.MIXED)) {
+                    return new ResponseEntity<>("Insufficient rights", HttpStatus.FORBIDDEN);
                 }
-            }
-            // Create template and serve html link
-            map.put("links", "/download/file/" + type + "/" + output);
-            if (json) {
-                ObjectMapper mapper = new ObjectMapper();
-                html = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(map);
-            } else {
-                html = getTemplate("downloadPdf.tpl");
-                map.put("file", output + "." + type);
-                s = new StringSubstitutor(map);
-                html = s.replace(html);
-            }
-            break;
+                if (al.equals(AccessLevel.FAIR_USE)) {
+                    output = idf.getImageGroupId() + "FAIR_USE:" + bPage.intValue() + "-" + ePage.intValue();// +"."+type;
+                } else {
+                    output = idf.getImageGroupId() + ":" + bPage.intValue() + "-" + ePage.intValue();// +"."+type;
+                }
+                if (type.equals(ArchiveBuilder.PDF_TYPE)) {
+                    Object pdf_cached = EHServerCache.IIIF.get(output);
+                    log.debug("PDF " + id + " from IIIF cache >>" + pdf_cached);
+                    if (pdf_cached == null) {
+                        // Build pdf since the pdf file doesn't exist yet
+                        ArchiveBuilder.buildPdf(accValidation.getAccess(), inf, idf, output,
+                                (String) request.getAttribute("origin"));
+                    }
+                }
+                if (type.equals(ArchiveBuilder.ZIP_TYPE)) {
+                    Object zip_cached = EHServerCache.IIIF_ZIP.get(output);
+                    log.debug("ZIP " + id + " from IIIF_ZIP cache >>" + zip_cached);
+                    if (zip_cached == null) {
+                        // Build pdf since the pdf file doesn't exist yet
+                        ArchiveBuilder.buildZip(accValidation.getAccess(), inf, idf, output,
+                                (String) request.getAttribute("origin"));
+                    }
+                }
+                // Create template and serve html link
+                map.put("links", Application.getProperty("iiifserv_baseurl") + "download/file/" + type + "/" + output);
+                if (json) {
+                    ObjectMapper mapper = new ObjectMapper();
+                    html = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(map);
+                } else {
+                    html = getTemplate("downloadPdf.tpl");
+                    map.put("file", output + "." + type);
+                    s = new StringSubstitutor(map);
+                    html = s.replace(html);
+                }
+                break;
         }
         if (json) {
             headers.setContentType(MediaType.parseMediaType("application/json"));
@@ -157,9 +164,10 @@ public class ArchivesController {
 
     }
 
-    @RequestMapping(value = "/download/file/{type}/{name}", method = { RequestMethod.GET, RequestMethod.HEAD })
-    public ResponseEntity<ByteArrayResource> downloadPdf(@PathVariable String name, @PathVariable String type) throws Exception {
-        log.info("downloadPdf(name {} , type {})",name,type);
+    @RequestMapping(value = "/download/file/{type}/{name}", method = {RequestMethod.GET, RequestMethod.HEAD})
+    public ResponseEntity<ByteArrayResource> downloadPdf(@PathVariable String name, @PathVariable String type)
+            throws Exception {
+        log.info("downloadPdf(name {} , type {})", name, type);
         byte[] array = null;
         if (type.equals(ArchiveBuilder.PDF_TYPE)) {
             array = (byte[]) EHServerCache.IIIF.get(name.substring(4));
@@ -172,19 +180,21 @@ public class ArchivesController {
         HttpHeaders headers = new HttpHeaders();
         if (array == null) {
             headers.setContentType(MediaType.parseMediaType("text/plain"));
-            array = new String("The link is wrong or has expired: please retry loading the archive and proceed to its download within 10 mn")
-                    .getBytes();
+            array = new String(
+                    "The link is wrong or has expired: please retry loading the archive and proceed to its download within 10 mn")
+                            .getBytes();
             return new ResponseEntity<ByteArrayResource>(new ByteArrayResource(array), headers, HttpStatus.NOT_FOUND);
         }
         headers.setContentType(MediaType.parseMediaType("application/" + type));
         headers.setContentDispositionFormData("attachment", name.substring(4) + "." + type);
-        ResponseEntity<ByteArrayResource> response = new ResponseEntity<ByteArrayResource>(new ByteArrayResource(array), headers, HttpStatus.OK);
+        ResponseEntity<ByteArrayResource> response = new ResponseEntity<ByteArrayResource>(new ByteArrayResource(array),
+                headers, HttpStatus.OK);
         return response;
     }
 
-    @RequestMapping(value = "/download/job/{type}/{id}", method = { RequestMethod.GET, RequestMethod.HEAD })
+    @RequestMapping(value = "/download/job/{type}/{id}", method = {RequestMethod.GET, RequestMethod.HEAD})
     public ResponseEntity<String> jobState(@PathVariable String id, @PathVariable String type) throws Exception {
-        log.info("jobState(id {}, type {})",id, type);
+        log.info("jobState(id {}, type {})", id, type);
         Identifier idf = new Identifier(id, Identifier.MANIFEST_ID);
         Integer bPage = idf.getBPageNum();
         if (bPage == null) {
@@ -195,7 +205,8 @@ public class ArchivesController {
             IdentifierInfo inf = new IdentifierInfo(idf.getImageGroupId());
             ePage = inf.getTotalPages();
         }
-        String url = "download/file/" + type + "/" + idf.getImageGroupId() + ":" + bPage + "-" + ePage + "." + type;
+        String url = Application.getProperty("iiifserv_baseurl") + "download/file/" + type + "/" + idf.getImageGroupId()
+                + ":" + bPage + "-" + ePage + "." + type;
         boolean done = false;
         if (type.equals(ArchiveBuilder.PDF_TYPE)) {
             done = ArchiveBuilder.isPdfDone(idf.getImageGroupId() + ":" + bPage + "-" + ePage + ".pdf");
@@ -238,8 +249,10 @@ public class ArchivesController {
             String s = vlist.get(i);
             String shortName = getShortName(s);
             IdentifierInfo vi = new IdentifierInfo("bdr:" + shortName);
-            links = links + "<a type=\"application/" + type + "\" href=\"/download/" + type + "/v:" + "bdr:" + shortName + "::1-" + vi.getTotalPages()
-                    + "\">Vol." + vi.igi.volumeNumber + " (" + vi.getTotalPages() + " pages) - " + "bdr:" + shortName + "." + type + "</a><br/>";
+            links = links + "<a type=\"application/" + type + "\" href=\"" + Application.getProperty("iiifserv_baseurl")
+                    + "download/" + type + "/v:" + "bdr:" + shortName + "::1-" + vi.getTotalPages() + "\">Vol."
+                    + vi.igi.volumeNumber + " (" + vi.getTotalPages() + " pages) - " + "bdr:" + shortName + "." + type
+                    + "</a><br/>";
         }
         return links;
     }
@@ -252,7 +265,8 @@ public class ArchivesController {
             String s = vlist.get(i);
             String shortName = getShortName(s);
             HashMap<String, String> vol = new HashMap<>();
-            vol.put("link", "/download/" + type + "/v:" + "bdr:" + shortName + "::1-");
+            vol.put("link", Application.getProperty("iiifserv_baseurl") + "download/" + type + "/v:" + "bdr:"
+                    + shortName + "::1-");
             vol.put("volume", Integer.toString(i));
             map.put("bdr:" + shortName, vol);
         }
