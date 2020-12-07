085bbb3f77e698773cc43fe6f658e4173625ce7d
MarcAgate <agate.marc@gmail.com>
2020-12-07 18:13:06 +0100

    Not using image cache for disk based image source

--- src/main/java/io/bdrc/iiif/image/service/ConcurrentResourceService.java
+++ src/main/java/io/bdrc/iiif/image/service/ConcurrentResourceService.java
@@ -10,6 +10,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.bdrc.iiif.core.Application;
 import io.bdrc.iiif.core.EHServerCache;
 import io.bdrc.iiif.exceptions.IIIFException;
 
@@ -19,6 +20,7 @@ public class ConcurrentResourceService<T> {
 
     String cachePrefix = "";
     String cacheName;
+    boolean skipImgCache = false;
 
     Map<String, CompletableFuture<T>> futures = new ConcurrentHashMap<>();
 
@@ -28,6 +30,7 @@ public class ConcurrentResourceService<T> {
     public ConcurrentResourceService(String cacheName, String cachePrefix) {
         this.cachePrefix = cachePrefix;
         this.cacheName = cacheName;
+        this.skipImgCache = skipImgCache();
     }
 
     @SuppressWarnings("unchecked")
@@ -62,6 +65,10 @@ public class ConcurrentResourceService<T> {
     @SuppressWarnings("unchecked")
     public T getSync(String resId) throws IIIFException {
         resId = normalizeId(resId);
+        if (skipImgCache) {
+            return getFromApi(resId);
+        }
+
         T resT = getFromCache(resId);
         if (resT != null) {
             logger.debug("found cache for " + resId);
@@ -77,42 +84,46 @@ public class ConcurrentResourceService<T> {
     }
 
     /*
-     * Function returning a CompletableFuture and thus allowing many different calls
-     * to iiif-presentation to try to access a non-cached WorkInfo at the same time
-     * while doing only one request.
+     * Function returning a CompletableFuture and thus allowing many different
+     * calls to iiif-presentation to try to access a non-cached WorkInfo at the
+     * same time while doing only one request.
      * 
-     * What it prevents is the following case. We assume that we're using a naive
-     * function, not this one. let's say an API call to lds-pdi takes 100ms. Let's
-     * say we ask for all the volumes of the Kangyur at the same time to
-     * iiif-presentation (something which actually happens). This means that the
-     * following happens: - first request to iiif-presentation: no workinfo cached,
-     * making a request to lds-pdi, takes 100ms - second request to
-     * iiif-presentation (2ms after the first one): the call to lds-pdi hasn't
-     * finished yet, so there is still no cache, so making another request to
-     * lds-pdi - etc.
+     * What it prevents is the following case. We assume that we're using a
+     * naive function, not this one. let's say an API call to lds-pdi takes
+     * 100ms. Let's say we ask for all the volumes of the Kangyur at the same
+     * time to iiif-presentation (something which actually happens). This means
+     * that the following happens: - first request to iiif-presentation: no
+     * workinfo cached, making a request to lds-pdi, takes 100ms - second
+     * request to iiif-presentation (2ms after the first one): the call to
+     * lds-pdi hasn't finished yet, so there is still no cache, so making
+     * another request to lds-pdi - etc.
      * 
      * which can lead to a large load of requests to lds-pdi, making all the
      * requests much longer and stressing lds-pdi.
      * 
-     * With this function the following happens (conceptually): - first request to
-     * iiif-presentation: no workinfo cached, making adding the request to lds-pdi
-     * in the map of requests - second request to iiif-presentation: no workinfo
-     * cached, but we see that the maps of requests contains one request for this
-     * resource, so we just wait for it to return - etc.
+     * With this function the following happens (conceptually): - first request
+     * to iiif-presentation: no workinfo cached, making adding the request to
+     * lds-pdi in the map of requests - second request to iiif-presentation: no
+     * workinfo cached, but we see that the maps of requests contains one
+     * request for this resource, so we just wait for it to return - etc.
      * 
      * which means just one request is made instead of 100.
      * 
      */
     public CompletableFuture<T> getAsync(String resId) {
         resId = normalizeId(resId);
-        T resT = getFromCache(resId);
+        T resT = null;
+        if (!skipImgCache) {
+            resT = getFromCache(resId);
+        }
         if (resT != null) {
             logger.debug("found cache for {}", resId);
             CompletableFuture<T> resCached = new CompletableFuture<>();
             resCached.complete(resT);
             return resCached;
         }
-        // unintuitive way to perform the (necessary) atomic operation in the list
+        // unintuitive way to perform the (necessary) atomic operation in the
+        // list
         CompletableFuture<T> res = new CompletableFuture<>();
         CompletableFuture<T> resFromList = futures.putIfAbsent(resId, res);
         if (resFromList != null) {
@@ -125,15 +136,23 @@ public class ConcurrentResourceService<T> {
         } catch (IIIFException e) {
             res.completeExceptionally(e);
             // this means that each failed fetch will not be saved and the next
-            // API call will trigger a new one... this could be optimized by doing
+            // API call will trigger a new one... this could be optimized by
+            // doing
             // that every, say 10mn, so that we don't do too many external calls
             futures.remove(resId);
             return res;
         }
-        putInCache(resId, resT);
+        if (!skipImgCache) {
+            putInCache(resId, resT);
+        }
         res.complete(resT);
         futures.remove(resId);
         return res;
+    }
+
+    private boolean skipImgCache() {
+        return (Application.getProperty("imageSourceType").equalsIgnoreCase(Application.DISK_SOURCE)
+                && cacheName.equals("iiif_img"));
     }
 
 }
