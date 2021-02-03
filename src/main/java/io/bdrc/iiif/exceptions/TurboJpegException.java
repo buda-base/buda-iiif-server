5a8656bdd4902c91a5ff688bfba9a500fbfbc054
eroux <elie.roux@telecom-bretagne.eu>
2021-02-03 09:25:44 +0100

    attempt to fix turbojpeg refusing to register

--- src/main/java/io/bdrc/iiif/exceptions/TurboJpegException.java
+++ src/main/java/io/bdrc/iiif/exceptions/TurboJpegException.java
@@ -1,6 +1,11 @@
 package io.bdrc.iiif.exceptions;
 
 public class TurboJpegException extends Exception {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1981239426743930178L;
+
     public TurboJpegException(String s) {
         super(s);
     }
