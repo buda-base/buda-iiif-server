f14b467ad6328078190fdd62f852d26ad3fefb70
eroux <elie.roux@telecom-bretagne.eu>
2021-04-16 14:51:33 +0200

    update deps

--- src/main/java/io/bdrc/iiif/resolver/IdentifierInfo.java
+++ src/main/java/io/bdrc/iiif/resolver/IdentifierInfo.java
@@ -100,7 +100,7 @@ public class IdentifierInfo {
         // work is fair use but user is authorized to see it in full
         // return full list
         log.info("USER HAS FAIR USE RESOURCE ACCESS {} and IS ADMIN {}", acc.hasResourceAccess(RdfConstants.FAIR_USE),
-                acc.getUser().isAdmin());
+                acc.getUserProfile().isAdmin());
         if (isFairUse() && acc.hasResourceAccess(RdfConstants.FAIR_USE)) {
             return info;
         }
