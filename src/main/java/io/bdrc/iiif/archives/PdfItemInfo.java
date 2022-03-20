9fa43efbf2bd8d420a89def4f8bd61badaaeccfe
eroux <elie.roux@telecom-bretagne.eu>
2022-03-20 16:57:26 +0100

    (minor) variable renaming

--- src/main/java/io/bdrc/iiif/archives/PdfItemInfo.java
+++ src/main/java/io/bdrc/iiif/archives/PdfItemInfo.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.NodeIterator;
+import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.ResourceFactory;
 import org.slf4j.Logger;
@@ -16,73 +17,78 @@ import org.slf4j.LoggerFactory;
 import io.bdrc.iiif.core.Application;
 import io.bdrc.iiif.core.EHServerCache;
 import io.bdrc.iiif.exceptions.IIIFException;
+import io.bdrc.libraries.Models;
 
 public class PdfItemInfo {
 
     private static final Logger log = LoggerFactory.getLogger(PdfItemInfo.class);
 
-    public String itemId;
-    public List<String> itemVolumes;
+    public String iinstanceQname;
+    public List<String> volumes;
     public HashMap<String, String> volNumbers;
-    public String itemAccess;
+    public String access;
+    public String status;
 
     public static String ITEM_URL_ROOT = Application.getProperty("dataserver") + "/query/graph/IIIFPres_imageInstanceGraph?R_RES=";
-    public static String BDO = "http://purl.bdrc.io/ontology/core/";
-    public static String BDR = "http://purl.bdrc.io/resource/";
-    public static String BDA = "http://purl.bdrc.io/admindata/";
-    public static String ADM = "http://purl.bdrc.io/ontology/admin/";
-    public static String TMP = "http://purl.bdrc.io/ontology/tmp/";
-    private Model itemModel;
+    private Model model;
 
-    public static PdfItemInfo getPdfItemInfo(String itemId) throws IIIFException {
-
-        PdfItemInfo meta = (PdfItemInfo) EHServerCache.PDF_ITEM_INFO.get(itemId + "_PdfItemInfo");
+    public static PdfItemInfo getPdfItemInfo(String iinstanceQname) throws IIIFException {
+        PdfItemInfo meta = (PdfItemInfo) EHServerCache.PDF_ITEM_INFO.get(iinstanceQname + "_PdfItemInfo");
         if (meta == null) {
-            meta = new PdfItemInfo(itemId);
-            EHServerCache.PDF_ITEM_INFO.put(itemId + "_PdfItemInfo", meta);
+            meta = new PdfItemInfo(iinstanceQname);
+            EHServerCache.PDF_ITEM_INFO.put(iinstanceQname + "_PdfItemInfo", meta);
         }
         return meta;
     }
 
-    private PdfItemInfo(String itemId) {
+    private PdfItemInfo(String iinstanceQname) {
         super();
-        this.itemModel = ModelFactory.createDefaultModel();
-        if (itemId != null) {
-            this.itemId = itemId;
-            log.info("PDF Info Url {}", ITEM_URL_ROOT + itemId + "&format=ttl");
-            itemModel.read(ITEM_URL_ROOT + itemId + "&format=ttl", "TURTLE");
+        this.model = ModelFactory.createDefaultModel();
+        if (iinstanceQname != null) {
+            this.iinstanceQname = iinstanceQname;
+            log.info("PDF Info Url {}", ITEM_URL_ROOT + iinstanceQname + "&format=ttl");
+            model.read(ITEM_URL_ROOT + iinstanceQname + "&format=ttl", "TURTLE");
         }
     }
 
-    public String getItemAccess() {
-        if (itemAccess == null) {
-            NodeIterator ni = itemModel.listObjectsOfProperty(ResourceFactory.createProperty(ADM + "access"));
-            itemAccess = ni.next().asNode().getURI();
-            itemAccess = itemAccess.substring(itemAccess.lastIndexOf('/') + 1);
+    public static final Property admAccess = ResourceFactory.createProperty(Models.ADM+"access");
+    public String getAccessLName() {
+        if (access == null) {
+            NodeIterator ni = model.listObjectsOfProperty(admAccess);
+            access = ni.next().asResource().getLocalName();
         }
-        return itemAccess;
+        return access;
+    }
+    
+    public static final Property admStatus = ResourceFactory.createProperty(Models.ADM+"status");
+    public String getStatusLName() {
+        if (status == null) {
+            NodeIterator ni = model.listObjectsOfProperty(admStatus);
+            status = ni.next().asResource().getLocalName();
+        }
+        return status;
     }
 
-    public List<String> getItemVolumes() {
+    public List<String> getVolumes() {
         List<Resource> nodes;
-        if (itemVolumes == null) {
-            itemVolumes = new ArrayList<>();
-            nodes = itemModel.listSubjectsWithProperty(ResourceFactory.createProperty(BDO + "volumeNumber")).toList();
+        if (volumes == null) {
+            volumes = new ArrayList<>();
+            nodes = model.listSubjectsWithProperty(ResourceFactory.createProperty(Models.BDO + "volumeNumber")).toList();
             for (Resource nd : nodes) {
-                itemVolumes.add(nd.asResource().getURI());
+                volumes.add(nd.asResource().getURI());
             }
         }
-        return itemVolumes;
+        return volumes;
     }
 
-    public HashMap<String, String> getItemVolumeNumbers() {
+    public HashMap<String, String> getVolumeNumbers() {
         if (volNumbers == null) {
             volNumbers = new HashMap<>();
-            List<String> itemVols = getItemVolumes();
+            List<String> itemVols = getVolumes();
             for (String vol : itemVols) {
                 String shortName = vol.substring(vol.lastIndexOf('/') + 1);
-                String num = itemModel
-                        .listObjectsOfProperty(ResourceFactory.createResource(vol), ResourceFactory.createProperty(BDO + "volumeNumber")).next()
+                String num = model
+                        .listObjectsOfProperty(ResourceFactory.createResource(vol), ResourceFactory.createProperty(Models.BDO + "volumeNumber")).next()
                         .asLiteral().getString();
                 volNumbers.put(shortName, num);
             }
@@ -90,22 +96,22 @@ public class PdfItemInfo {
         return volNumbers;
     }
 
-    public String getItemVolumeNumber(String volumeId) {
-        return getItemVolumeNumbers().get(volumeId);
+    public String getVolumeNumber(String volumeId) {
+        return getVolumeNumbers().get(volumeId);
     }
 
     @Override
     public String toString() {
-        return "PdfItemInfo [itemId=" + itemId + ", itemVolumes=" + itemVolumes + ", volNumbers=" + volNumbers + ", itemAccess=" + itemAccess
-                + ", itemModel=" + itemModel + "]";
+        return "PdfItemInfo [itemId=" + iinstanceQname + ", itemVolumes=" + volumes + ", volNumbers=" + volNumbers + ", itemAccess=" + access
+                + ", itemModel=" + model + "]";
     }
 
     public static void main(String[] args) throws MalformedURLException, IIIFException {
         PdfItemInfo pdf = PdfItemInfo.getPdfItemInfo("bdr:I23703");
-        System.out.println(pdf.getItemVolumes());
-        System.out.println(pdf.getItemVolumeNumbers());
-        System.out.println(pdf.getItemVolumeNumber("V23703_I1529"));
-        System.out.println(pdf.getItemAccess());
+        System.out.println(pdf.getVolumes());
+        System.out.println(pdf.getVolumeNumbers());
+        System.out.println(pdf.getVolumeNumber("V23703_I1529"));
+        System.out.println(pdf.getAccessLName());
     }
 
 }
