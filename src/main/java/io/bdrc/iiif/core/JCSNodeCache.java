94dc5c67fb0163bd4883d6208c6f43d335054ac8
eroux <elie.roux@telecom-bretagne.eu>
2022-05-10 10:02:31 +0200

    update cache mechanism

--- src/main/java/io/bdrc/iiif/core/JCSNodeCache.java
+++ src/main/java/io/bdrc/iiif/core/JCSNodeCache.java
@@ -11,33 +11,33 @@ import org.ehcache.config.units.EntryUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import com.maxmind.db.CacheKey;
+import com.maxmind.db.DecodedValue;
 import com.maxmind.db.NodeCache;
 
 public class JCSNodeCache implements NodeCache {
 
     // The raison d'être of this class is to be used by Geoloc's cache mechanism
 
-    public static Cache<String, JsonNode> CACHE;
+    public static Cache<CacheKey, DecodedValue> CACHE;
     public final static Logger log = LoggerFactory.getLogger(JCSNodeCache.class.getName());
 
     public JCSNodeCache() {
         CacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder().build();
         cacheManager.init();
-        CACHE = cacheManager.createCache("geoloc", CacheConfigurationBuilder.newCacheConfigurationBuilder(String.class, JsonNode.class,
+        CACHE = cacheManager.createCache("geoloc", CacheConfigurationBuilder.newCacheConfigurationBuilder(CacheKey.class, DecodedValue.class,
                 ResourcePoolsBuilder.newResourcePoolsBuilder().heap(500, EntryUnit.ENTRIES)));
         log.debug("Cache was initialized {}", CACHE);
         //cacheManager.close();
     }
 
     @Override
-    public JsonNode get(int key, Loader loader) throws IOException {
+    public DecodedValue get(CacheKey key, Loader loader) throws IOException {
         try {
-            String k = Integer.toString(key);
-            JsonNode value = CACHE.get(k);
+            DecodedValue value = CACHE.get(key);
             if (value == null) {
                 value = loader.load(key);
-                CACHE.put(k, value);
+                CACHE.put(key, value);
             }
             return value;
         } catch (IOException e) {
